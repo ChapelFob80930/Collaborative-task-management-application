b5698cf7b95b839038b2bbac656b9f1c937ebe1b
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2024-12-23 18:17:57 +0100

    Backend classes created

--- src/main/java/Backend/Employee.java
+++ src/main/java/Backend/Employee.java
@@ -3,8 +3,8 @@ package Backend;
 import java.sql.*;
 
 public class Employee {
-    public static void main(String[] args) {
-
-
-    }
+//    public static void main(String[] args) {
+//
+//
+//    }
 }
