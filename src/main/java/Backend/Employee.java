6fb7a0c9aa6b4fadb0cd4fd9552dc7dd31182d3c
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2024-12-23 13:19:47 +0100

    added insert functionality for database

--- src/main/java/Backend/Employee.java
+++ src/main/java/Backend/Employee.java
@@ -11,6 +11,19 @@ public class Employee {
                     "root"
             );
 
+
+            String sql = "INSERT INTO employees (employeeId, name, email, role, projectId) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+
+
+            pstmt.setInt(1,102);
+            pstmt.setString(2,"insertTestName");
+            pstmt.setString(3,"insertTestEmail");
+            pstmt.setString(4,"insertTestRole");
+            pstmt.setInt(5,103);
+            int rowsInserted = pstmt.executeUpdate(); 
+            System.out.println("Rows inserted: " + rowsInserted);
+
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM employees");
 
