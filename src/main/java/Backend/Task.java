63c0e32c0c5eb36e21efbe4e341f52693317d220
Thrinath008 <148782730+Thrinath008@users.noreply.github.com>
2024-12-24 13:23:57 +0100

    created constructor for task classes

--- src/main/java/Backend/Task.java
+++ src/main/java/Backend/Task.java
@@ -1,5 +1,6 @@
 package Backend;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -11,8 +12,22 @@ public class Task {
     private  String status;
     private String category;
     private Date dueDate;
-//    List<Employee>
+    List<Employee> tasks;
+
+
+    public Task(int taskId, String title, String description, String priority, String status, String category, Date dueDate) {
+        this.taskId = taskId;
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.status = status;
+        this.category = category;
+        this.dueDate = dueDate;
+        List<Employee> tasks = new ArrayList<>();
+    }
+
     private void createTask(){
+
 
     }
     private  void updateTask(){
